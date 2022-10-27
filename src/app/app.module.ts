60ac28ee0499e3db29583da731c16751a76a8171
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-10-27 12:53:14 -0500

    clock v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VerticalBarComponent } from './components/vertical-bar/vertical-bar.component';
 import { PrincipalComponent } from './views/principal/principal.component';
+import { ClockV1Component } from './components/clock-v1/clock-v1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VerticalBarComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    ClockV1Component
   ],
   imports: [
     BrowserModule,
